a570eb04f1de7dc9f92de5ea392373f98c79f7a9
lhqing <liuhq@ucsd.edu>
2019-08-07 13:15:46 -0700

    add ame

--- ALLCools/motif/ame.py
+++ ALLCools/motif/ame.py
@@ -1,3 +1,60 @@
+import pathlib
 from .utilities import get_fasta
 
+def parallel_ame(bed_files, background_files, motif_files,
+                 output_dir, reference_fasta, cpu=1, slop_b=50):
+    """
+    Run motif enrichment analysis on EACH bed_file with ALL the motif_files,
+    using ALL background_files (merged) as control region
 
+    Parameters
+    ----------
+    bed_files
+    background_files
+    motif_files
+    cpu
+
+    Returns
+    -------
+
+    """
+    output_dir = pathlib.Path(output_dir)
+    if isinstance(bed_files, str):
+        bed_files = [bed_files]
+    if isinstance(background_files, str):
+        background_files = [background_files]
+
+    # get bed FASTQ
+    fasta_list = []
+    for bed_file in bed_files:
+        bed_file = pathlib.Path(bed_file)
+        output_fasta_path = output_dir / (bed_file.name + '.fasta')
+        get_fasta([bed_file],
+                  fasta_path=reference_fasta,
+                  output_path=output_fasta_path,
+                  slop_b=slop_b,
+                  chrom_size_path=None,
+                  cpu=cpu, sort_mem_gbs=min(int(cpu * 3), 30))
+        fasta_list.append(output_fasta_path)
+
+    # get merged background FASTQ
+    control_fasta = output_dir / 'CONTROL_REGIONS.fasta'
+    get_fasta(background_files,
+              fasta_path=reference_fasta,
+              output_path=control_fasta,
+              slop_b=slop_b,
+              chrom_size_path=None,
+              cpu=cpu, sort_mem_gbs=min(int(cpu * 3), 30))
+
+    # split motif files based on cpu
+
+
+    # run single_bed_single_motif_runner
+
+    # merge results
+
+    return
+
+
+def single_bed_single_motif_runner():
+    return
--- ALLCools/motif/utilities.py
+++ ALLCools/motif/utilities.py
@@ -2,6 +2,7 @@ import pathlib
 import shlex
 import subprocess
 
+import numpy as np
 from pybedtools import BedTool, cleanup
 
 from .._open import open_gz
@@ -43,8 +44,13 @@ def get_fasta(bed_file_paths, fasta_path, output_path, slop_b=None, chrom_size_p
                 temp_f.write(f.read())
 
     sorted_temp = output_path + '.tmp_sorted.bed'
-    subprocess.run(shlex.split(f'sort -k1,1 -k2,2n --parallel={cpu} -S {sort_mem_gbs}G {temp_bed} -o {sorted_temp}'),
-                   stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf8', check=True)
+    try:
+        subprocess.run(
+            shlex.split(f'sort -k1,1 -k2,2n --parallel={cpu} -S {sort_mem_gbs}G {temp_bed} -o {sorted_temp}'),
+            stderr=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf8', check=True)
+    except subprocess.CalledProcessError as e:
+        print(e.stderr)
+        raise e
 
     sorted_bed = BedTool(sorted_temp)
     if slop_b:
@@ -60,3 +66,67 @@ def get_fasta(bed_file_paths, fasta_path, output_path, slop_b=None, chrom_size_p
     subprocess.run(shlex.split(f'rm -f {temp_bed} {sorted_temp} {merged_temp}'))
     cleanup()
     return output_path
+
+
+def meme_to_homer(meme_path, homer_path, score_power=0.85):
+    """
+    Transfer MEME motif format into Homer motif format.
+    Based on description here: http://homer.ucsd.edu/homer/motif/creatingCustomMotifs.html
+    The score_power controls Log odds detection threshold by max_score ** score_power
+
+    Parameters
+    ----------
+    meme_path
+        Input path in meme format
+    homer_path
+        Output path in homer format
+    score_power
+        Log odds detection threshold = max_score ** score_power
+    Returns
+    -------
+
+    """
+
+    if score_power >= 1:
+        raise ValueError('score_power must < 1')
+
+    in_motif = False
+    records = []
+    with open(meme_path) as f:
+        for line in f:
+            if line.startswith('MOTIF'):
+                if in_motif:
+                    records.append(cur_char)
+                    cur_char = ''
+                else:
+                    in_motif = True
+                    cur_char = ''
+            if not in_motif:
+                continue
+            else:
+                cur_char += line
+
+    with open(homer_path, 'w') as f:
+        for record in records:
+            lines = record.split('\n')
+            head_line = lines[0]
+            matrix_line = []
+            enter_matrix = False
+            for line in lines:
+                if line.startswith('letter-probability matrix'):
+                    enter_matrix = True
+                    continue
+
+                ll = line.strip().split('  ')
+                ll = [i.strip() for i in ll]
+                if enter_matrix and len(ll) == 4:
+                    matrix_line.append(ll)
+                else:
+                    continue
+            matrix = np.array(matrix_line).astype(float)
+            best_score = np.log(matrix.max(axis=1) / 0.25).sum()
+            uid = head_line.split(' ')[1]
+            homer_head_line = f'>\t{uid}\t{best_score ** score_power}\n'
+            matrix_line = '\n'.join(['\t'.join(line) for line in matrix_line]) + '\n'
+            f.write(homer_head_line + matrix_line)
+    return
