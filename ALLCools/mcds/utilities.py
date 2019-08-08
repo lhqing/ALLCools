7059ed0d1a7afd61511c25fa771ab6edb516976a
lhqing <liuhq@ucsd.edu>
2019-08-08 12:48:17 -0700

    highly variable feature

--- ALLCools/mcds/utilities.py
+++ ALLCools/mcds/utilities.py
@@ -1,4 +1,9 @@
+import logging
+import numpy as np
+import pandas as pd
 import xarray as xr
+
+log = logging.getLogger()
 
 
 def calculate_posterior_mc_rate(mc_da,
@@ -102,3 +107,127 @@ def calculate_gch_rate(mcds, var_dim='chrom100k'):
     overall_gchn = (gchn - hchn) / (1 - hchn)
     real_gc_rate = real_gc_rate / overall_gchn.values[:, None]
     return real_gc_rate
+
+
+def get_mean_dispersion(x, obs_dim, var_dim):
+    # mean
+    mean = x.mean(dim=var_dim)
+
+    # var
+    mean_sq = (x * x).mean(dim=var_dim)
+    # enforce R convention (unbiased estimator) for variance
+    var = (mean_sq - mean ** 2) * (x.sizes[obs_dim] / (x.sizes[obs_dim] - 1))
+
+    # now actually compute the dispersion
+    mean.where(mean > 1e-12, 1e-12)  # set entries equal to zero to small value
+    # raw dispersion is the variance normalized by mean
+    dispersion = var / mean
+
+    mean.load()
+    dispersion.load()
+    return mean, dispersion
+
+
+def highly_variable_methylation_feature(
+        cell_by_feature_matrix, feature_mean_cov,
+        obs_dim=None, var_dim=None,
+        min_disp=0.5, max_disp=None,
+        min_mean=0, max_mean=5,
+        n_top_feature=None, bin_min_features=5,
+        mean_binsize=0.05, cov_binsize=100):
+    """
+    Adapted from Scanpy, see license above
+    The main difference is that, this function normalize dispersion based on both mean and cov bins.
+    """
+    # RNA is only scaled by mean, but methylation is scaled by both mean and cov
+    log.info('extracting highly variable features')
+
+    if n_top_feature is not None:
+        log.info('If you pass `n_top_feature`, all cutoffs are ignored.')
+
+    # warning for extremely low cov
+    low_cov_portion = (feature_mean_cov < 30).sum() / feature_mean_cov.size
+    if low_cov_portion > 0.2:
+        log.warning(f'{int(low_cov_portion * 100)}% feature with < 10 mean cov, '
+                    f'consider filter by cov before find highly variable feature. '
+                    f'Otherwise some low coverage feature may be elevated after normalization.')
+
+    if len(cell_by_feature_matrix.dims) != 2:
+        raise ValueError(f'Input cell_by_feature_matrix is not 2-D matrix, '
+                         f'got {len(cell_by_feature_matrix.dims)} dim(s)')
+    else:
+        if (obs_dim is None) or (var_dim is None):
+            obs_dim, var_dim = cell_by_feature_matrix.dims
+
+    # rename variable
+    x = cell_by_feature_matrix
+    cov = feature_mean_cov
+
+    mean, dispersion = get_mean_dispersion(x, obs_dim=obs_dim, var_dim=var_dim)
+    dispersion = np.log(dispersion)
+
+    # all of the following quantities are "per-feature" here
+    df = pd.DataFrame(index=cell_by_feature_matrix.get_index(var_dim))
+    df['mean'] = mean.to_pandas()
+    df['dispersion'] = dispersion.to_pandas()
+    df['cov'] = cov
+
+    # instead of n_bins, use bin_size, because cov and mc are in different scale
+    df['mean_bin'] = (df['mean'] / mean_binsize).astype(int)
+    df['cov_bin'] = (df['cov'] / cov_binsize).astype(int)
+
+    # save bin_count df, gather bins with more than bin_min_features features
+    bin_count = df.groupby(['mean_bin', 'cov_bin']) \
+        .apply(lambda i: i.shape[0]) \
+        .reset_index() \
+        .sort_values(0, ascending=False)
+    bin_count.head()
+    bin_more_than = bin_count[bin_count[0] > bin_min_features]
+    if bin_more_than.shape[0] == 0:
+        raise ValueError(f'No bin have more than {bin_min_features} features, uss larger bin size.')
+
+    # for those bin have too less features, merge them with closest bin in manhattan distance
+    # this usually don't cause much difference (a few hundred features), but the scatter plot will look more nature
+    index_map = {}
+    for _, (mean_id, cov_id, count) in bin_count.iterrows():
+        if count > 1:
+            index_map[(mean_id, cov_id)] = (mean_id, cov_id)
+        manhattan_dist = (bin_more_than['mean_bin'] - mean_id).abs() + (bin_more_than['cov_bin'] - cov_id).abs()
+        closest_more_than = manhattan_dist.sort_values().index[0]
+        closest = bin_more_than.loc[closest_more_than]
+        index_map[(mean_id, cov_id)] = tuple(closest.tolist()[:2])
+    # apply index_map to original df
+    raw_bin = df[['mean_bin', 'cov_bin']].set_index(['mean_bin', 'cov_bin'])
+    raw_bin['use_mean'] = pd.Series(index_map).apply(lambda i: i[0])
+    raw_bin['use_cov'] = pd.Series(index_map).apply(lambda i: i[1])
+    df['mean_bin'] = raw_bin['use_mean'].values
+    df['cov_bin'] = raw_bin['use_cov'].values
+
+    # calculate bin mean and std, now disp_std_bin shouldn't have NAs
+    disp_grouped = df.groupby(['mean_bin', 'cov_bin'])['dispersion']
+    disp_mean_bin = disp_grouped.mean()
+    disp_std_bin = disp_grouped.std(ddof=1)
+
+    # actually do the normalization
+    _mean_norm = disp_mean_bin.loc[list(zip(df['mean_bin'], df['cov_bin']))]
+    _std_norm = disp_std_bin.loc[list(zip(df['mean_bin'], df['cov_bin']))]
+    df['dispersion_norm'] = (df['dispersion'].values  # use values here as index differs
+                             - _mean_norm.values) / _std_norm.values
+    dispersion_norm = df['dispersion_norm'].values.astype('float32')
+
+    # Select n_top_feature
+    if n_top_feature is not None:
+        dispersion_norm = dispersion_norm[~np.isnan(dispersion_norm)]
+        dispersion_norm[::-1].sort()  # interestingly, np.argpartition is slightly slower
+        disp_cut_off = dispersion_norm[n_top_feature - 1]
+        gene_subset = np.nan_to_num(df['dispersion_norm'].values) >= disp_cut_off
+        log.info(f'the {n_top_feature} top genes correspond to a normalized dispersion cutoff of {disp_cut_off}')
+    else:
+        max_disp = np.inf if max_disp is None else max_disp
+        dispersion_norm[np.isnan(dispersion_norm)] = 0  # similar to Seurat
+        gene_subset = np.logical_and.reduce((mean > min_mean, mean < max_mean,
+                                             dispersion_norm > min_disp,
+                                             dispersion_norm < max_disp))
+    df['gene_subset'] = gene_subset
+    log.info('    finished')
+    return df
