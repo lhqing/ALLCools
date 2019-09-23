a2c8c1b06289c2e159cbd4ee3b72d57e010b90ba
lhqing <liuhq@ucsd.edu>
2019-09-23 14:10:32 -0700

    Create dendrogram.py

---
+++ ALLCools/motif/dendrogram.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def extract_all_nodes(linkage, labels=None):
+    cluster_dict = {}
+    cur_cluster_id = len(linkage) + 1
+    total_obs = len(linkage) + 1
+    if labels is None:
+        labels = list(range(total_obs))
+    for left, right, distance, n_obs in linkage:
+        left = int(left)
+        right = int(right)
+        n_obs = int(n_obs)
+
+        cluster_dict[cur_cluster_id] = {'left': set(),
+                                        'right': set()}
+        if n_obs == 2:
+            left = labels[left]
+            right = labels[right]
+            # merge of 2 original observations
+            cluster_dict[cur_cluster_id]['left'].add(left)
+            cluster_dict[cur_cluster_id]['right'].add(right)
+        else:
+            # left and/or right are cluster
+            if left < total_obs:
+                left = labels[left]
+                cluster_dict[cur_cluster_id]['left'].add(left)
+            else:
+                # node are cluster
+                cluster_dict[cur_cluster_id]['left'].update(cluster_dict[left]['left'])
+                cluster_dict[cur_cluster_id]['left'].update(cluster_dict[left]['right'])
+            if right < total_obs:
+                right = labels[right]
+                cluster_dict[cur_cluster_id]['right'].add(right)
+            else:
+                # node are cluster
+                cluster_dict[cur_cluster_id]['right'].update(cluster_dict[right]['left'])
+                cluster_dict[cur_cluster_id]['right'].update(cluster_dict[right]['right'])
+        cur_cluster_id += 1
+    return cluster_dict
+
+
+def dmr_distance(a: np.ndarray, b: np.ndarray):
+    overlap = (a == b)[(a * b) != 0]
+    return 1 - (overlap.sum() / overlap.size)
