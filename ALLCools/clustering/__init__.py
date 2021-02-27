16c48aeccaf6f068994efe3cb6990b4da7d8b052
hq <liuhq@ucsd.edu>
2021-02-26 22:34:06 -0800

    Update documentation

--- ALLCools/clustering/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ConsensusClustering import ConsensusClustering
-from .art_of_tsne import tsne
-from .balanced_pca import balanced_pca
-from .scrublet import MethylScrublet
-from .pvclust import Dendrogram
-from .dmg import PairwiseDMG, aggregate_pairwise_dmg
