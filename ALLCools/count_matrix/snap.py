d7aa99e2d58a780985e9c589dd0e90bc1bcd897a
jksr <jksr.tw@gmail.com>
2021-09-21 16:42:01 -0700

    Update snap.py
    include read gene data from snap file

--- ALLCools/count_matrix/snap.py
+++ ALLCools/count_matrix/snap.py
@@ -4,9 +4,43 @@ import pandas as pd
 import numpy as np
 import scipy.sparse as ss
 
+def read_snap(file_path, bin_kind=5000):
+    """Read snap hdf5 file into anndata.AnnData. """
+    if bin_kind=='gene':
+        return _read_snap_genes(file_path)
+    elif isinstance(bin_kind, int):
+        return _read_snap_bins(file_path, bin_size=bin_kind)
+    else:
+        raise NotImplementedError
+    
 
-def read_snap(file_path, bin_size=5000):
-    """Read snap hdf5 file into anndata.AnnData"""
+def _read_snap_genes(file_path):
+    """Read gene data from snap hdf5 file into anndata.AnnData"""
+    
+    with h5py.File(file_path) as f:
+        data = np.array(f[f'/GM/count'])
+        idx = np.array(f[f'/GM/idx']) - 1  # R is 1 based, python is 0 based
+        idy = np.array(f[f'/GM/idy']) - 1  # R is 1 based, python is 0 based
+        gene_id = np.array(f[f'/GM/name']).astype(str)
+
+        cell_barcode = np.array(f['/BD/name']).astype(str)
+        cell_id = [f'ATAC_{i}' for i in range(cell_barcode.size)]
+        cell_meta = pd.DataFrame([], index=cell_id)
+        cell_meta['barcode'] = cell_barcode
+        for name in f['/BD/'].keys():
+            if name == 'name':
+                continue
+            cell_meta[name] = np.array(f[f'/BD/{name}'])
+        data = ss.coo_matrix((data, (idx, idy)), shape=(cell_barcode.size, gene_id.size)).tocsc()
+
+    adata = anndata.AnnData(X=data,
+                            obs=cell_meta,
+                            var=pd.DataFrame(index=pd.Index(gene_id, name='gene')))
+    return adata
+
+
+def _read_snap_bins(file_path, bin_size=5000):
+    """Read bin data from snap hdf5 file into anndata.AnnData"""
     with h5py.File(file_path) as f:
         data = np.array(f[f'/AM/{bin_size}/count'])
         idx = np.array(f[f'/AM/{bin_size}/idx']) - 1  # R is 1 based, python is 0 based
