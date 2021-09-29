b5669c51b82b879e59a2e7f271df4f570f384eea
jksr <jksr.tw@gmail.com>
2021-09-29 13:17:54 -0700

    pseudo_cell_kmeans are now scalable

--- ALLCools/pseudo_cell/pseudo_cell_kmeans.py
+++ ALLCools/pseudo_cell/pseudo_cell_kmeans.py
@@ -4,79 +4,62 @@ from sklearn.cluster import MiniBatchKMeans
 from scipy.sparse import issparse, csr_matrix, vstack
 import anndata
 
+def _kmeans_division(matrix, cells, max_pseudo_size, max_k=50):
+    labels = pd.Series(index=cells)
+    to_process = [(cells,'')]
+    while len(to_process)>0:
+        curr_cells, curr_prefix = to_process.pop()
+        curr_matrix = matrix[cells.isin(curr_cells)]
 
-def _kmeans_iter(matrix, cells, max_group_size, k=None, parent_label=None):
-    """Iteratively split matrix using kmeans until all the group size < max_group_size"""
-    if max_group_size <= 1:
-        return pd.Series(range(cells.size), index=cells).astype(str)
-    if k is None:
-        k = matrix.shape[0] // max_group_size + 1
-    if matrix.shape[0] <= k:
-        return pd.Series(np.ones_like(cells), index=cells).astype(str)
+        # MiniBatchKMeans causes seg fault with huge k; bound with max_k
+        k = min(len(curr_cells)//max_pseudo_size+1, max_k)
 
-    # iteratively split cells until all the
-    mbk = MiniBatchKMeans(n_clusters=k,
-                          init='k-means++',
-                          max_iter=100,
-                          batch_size=100,
-                          verbose=0,
-                          compute_labels=True,
-                          random_state=0,
-                          tol=0.0,
-                          max_no_improvement=10,
-                          init_size=3 * k,
-                          n_init=5,
-                          reassignment_ratio=0.1)
-    mbk.fit(matrix)
-    labels = pd.Series(mbk.labels_, index=cells).astype(str)
-    if parent_label is not None:
-        labels = labels.apply(lambda i: f'{parent_label}|{i}')
+        mbk = MiniBatchKMeans(n_clusters=k,
+                              init='k-means++',
+                              max_iter=100,
+                              batch_size=100,
+                              verbose=0,
+                              compute_labels=True,
+                              random_state=0,
+                              tol=0.0,
+                              max_no_improvement=10,
+                              init_size=3 * k,
+                              n_init=5,
+                              reassignment_ratio=0.1)
 
-    records = []
-    for label, group in labels.groupby(labels):
-        if group.size <= max_group_size:
-            records.append(group)
-        else:
-            # group is over sized, keep splitting
-            # get matrix of this group
-            group_matrix = matrix[cells.isin(group.index), :]
-            group_cells = group.index
-            new_k = group_matrix.shape[0] // max_group_size + 1
-            records += _kmeans_iter(matrix=group_matrix,
-                                    cells=group_cells,
-                                    k=new_k,
-                                    max_group_size=max_group_size,
-                                    parent_label=label)
-    if parent_label is None:
-        # the top function call
-        records = pd.concat(records)
-        rename_cluster = {c: str(i) for i, c in enumerate(records.value_counts().index)}
-        records = records.map(rename_cluster).sort_index()
-    return records
+        mbk.fit(curr_matrix)
+        curr_labels = curr_prefix+'|'+pd.Series(mbk.labels_).astype(str)
+        labels.loc[curr_cells] = curr_labels.tolist()
+
+        for cluster_label, cluster_cells in labels.loc[curr_cells].groupby(labels.loc[curr_cells]):
+            if cluster_cells.size <= max_pseudo_size:
+                continue
+            else:
+                to_process.append((cluster_cells.index, cluster_label))
+    return labels
 
 
-def _calculate_cell_group(clusters, total_matrix, cluster_size_cutoff=100, max_group_size=25):
+def _calculate_pseudo_group(clusters, total_matrix, pseudoable_cluster_size=100, max_pseudo_size=25):
     cluster_cells = clusters.value_counts()
-    max_group_sizes = (cluster_cells // cluster_size_cutoff + 1).astype(int)
-    max_group_sizes[max_group_sizes > max_group_size] = max_group_size
+    max_pseudo_sizes = (cluster_cells // pseudoable_cluster_size + 1).astype(int)
+    max_pseudo_sizes[max_pseudo_sizes > max_pseudo_size] = max_pseudo_size
 
     records = []
-    for cluster, max_group_size in max_group_sizes.items():
+    for cluster, max_pseudo_size in max_pseudo_sizes.items():
         cells = clusters[clusters == cluster].index
         matrix = total_matrix[clusters == cluster].copy()
-        record = _kmeans_iter(matrix, cells, max_group_size, k=None, parent_label=None)
+        record = _kmeans_division(matrix, cells, max_pseudo_size)
         record = record.apply(lambda i: f'{cluster}::{i}')
         records.append(record)
     records = pd.concat(records)
     return records
 
-
-def _merge_pseudo_cell(adata, aggregate_func='downsample'):
+def _merge_pseudo_cell(adata, aggregate_func, pseudo_group_key):
     is_sparse = issparse(adata.X)
     # merge ia to balanced ia (bia)
     balanced_matrix = []
     obs = {}
-    for group, cells in adata.obs_names.groupby(adata.obs['cell_group']).items():
+    for group, cells in adata.obs_names.groupby(adata.obs[pseudo_group_key]).items():
         n_cells = cells.size
         if cells.size == 1:
             group_data = adata.var_vector(cells[0]).ravel()
@@ -116,21 +99,26 @@ def _merge_pseudo_cell(adata, aggregate_func='downsample'):
     return pseudo_cell_adata
 
 
+
 def generate_pseudo_cells(adata,
                           cluster_col='leiden',
                           obsm='X_pca',
-                          cluster_size_cutoff=100,
-                          max_group_size=25,
-                          aggregate_func='downsample'):
+                          pseudoable_cluster_size=100,
+                          max_pseudo_size=25,
+                          aggregate_func='downsample',):
+
+    pseudo_group_key='pseudo_group'
+
     # determine cell group
     clusters = adata.obs[cluster_col]
     total_matrix = adata.obsm[obsm]
-    cell_group = _calculate_cell_group(clusters=clusters,
-                                       total_matrix=total_matrix,
-                                       cluster_size_cutoff=cluster_size_cutoff,
-                                       max_group_size=max_group_size)
-    adata.obs['cell_group'] = cell_group
+    pseudo_group = _calculate_pseudo_group(clusters=clusters,
+                                           total_matrix=total_matrix,
+                                           pseudoable_cluster_size=pseudoable_cluster_size,
+                                           max_pseudo_size=max_pseudo_size)
+    adata.obs[pseudo_group_key] = pseudo_group
     pseudo_cell_adata = _merge_pseudo_cell(adata=adata,
-                                           aggregate_func=aggregate_func)
-    pseudo_cell_adata.obs[cluster_col] = pseudo_cell_adata.obs_names.str.split('::').str[0]
+                                           aggregate_func=aggregate_func,
+                                           pseudo_group_key=pseudo_group_key,)
+    pseudo_cell_adata.obs[cluster_col] = pseudo_cell_adata.obs_names.str.split('::').str[:-1].str.join('::')
     return pseudo_cell_adata
