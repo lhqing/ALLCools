5b110954fd4a17f68917b92af0efa5b40ff0a9c0
hq <liuhq@ucsd.edu>
2022-06-25 15:18:13 -0700

    update default dot size

--- ALLCools/plot/utilities.py
+++ ALLCools/plot/utilities.py
@@ -215,30 +215,36 @@ def _auto_size(ax, n_dots):
     scale = bbox.width * bbox.height / 14.6  # 14.6 is a 5*5 fig I used to estimate
     n = n_dots / scale  # larger figure means data look sparser
     if n < 500:
-        s = 10 - n / 100
+        s = 14 - n / 100
     elif n < 1500:
-        s = 4
+        s = 7
     elif n < 3000:
-        s = 3
+        s = 5
     elif n < 8000:
-        s = 2
+        s = 3
     elif n < 15000:
-        s = 1
+        s = 2
     elif n < 30000:
-        s = 0.8
+        s = 1.5
+    elif n < 50000:
+        s = 1
     elif n < 80000:
-        s = 0.5
+        s = 0.8
     elif n < 150000:
-        s = 0.4
+        s = 0.6
     elif n < 300000:
-        s = 0.3
+        s = 0.5
     elif n < 500000:
-        s = 0.2
+        s = 0.4
+    elif n < 800000:
+        s = 0.3
     elif n < 1000000:
-        s = 0.1
+        s = 0.2
     elif n < 2000000:
-        s = 0.07
+        s = 0.1
     elif n < 3000000:
+        s = 0.07
+    elif n < 4000000:
         s = 0.05
     elif n < 5000000:
         s = 0.03
