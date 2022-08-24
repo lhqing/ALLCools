048a123c5b1b483024e8c180f163be060ac35fca
hq <liuhq@ucsd.edu>
2022-08-23 18:53:52 -0700

    Update __init__.py

--- ALLCools/__init__.py
+++ ALLCools/__init__.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
 
 __version__ = version("allcools")
