a0265764fa264e959d0d50e72df2f41df934ffc2
hq <liuhq@ucsd.edu>
2022-09-29 19:31:30 -0700

    Update cistarget.py

--- ALLCools/motif/cistarget.py
+++ ALLCools/motif/cistarget.py
@@ -60,7 +60,7 @@ def cistarget_motif_enrichment(rank_df, total_regions, auc_threshold=0.005, nes_
     if sum(enriched_features_idx) == 0:
         print("No enriched motifs found")
         motif_enrichment = pd.DataFrame([], columns=["NES", "AUC", "rank_at_max", "n_motif_hit"])
-        motif_hits = pd.DataFrame([], columns=rank_df.columns)
+        motif_hits = pd.DataFrame([], columns=rank_df.columns, dtype=bool)
         return motif_enrichment, motif_hits
 
     # Make dataframe
